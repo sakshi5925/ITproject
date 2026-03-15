8c5c74559799e8132de261b57f7383290f952752
sakshi5925 <sn125798@gmail.com>
2026-03-15 10:26:27 +0530

    fix cors

--- backend/server.js
+++ backend/server.js
@@ -16,17 +16,34 @@ const server = createServer(app);
 
 app.use(express.json());
 
+// const corsOptions = {
+//   origin: [
+//     "http://localhost:5173",
+//     "https://i-tproject-nine.vercel.app"
+//   ],
+//   methods: ["GET","POST","PUT","DELETE"],
+//   credentials: true
+// };
+
+// app.use(cors(corsOptions));
+// app.options("*", cors(corsOptions));
+
+
+
+
+
+const allowedOrigins = [
+  "http://localhost:5173",
+  "https://i-tproject-nine.vercel.app"
+];
+
 const corsOptions = {
-  origin: [
-    "http://localhost:5173",
-    "https://i-tproject-nine.vercel.app"
-  ],
-  methods: ["GET","POST","PUT","DELETE"],
-  credentials: true
+  origin: allowedOrigins,
+  credentials: true,
+  methods: ["GET","POST","PUT","DELETE","OPTIONS"]
 };
 
 app.use(cors(corsOptions));
-app.options("*", cors(corsOptions));
 
 app.use('/auth', authRoutes);
 app.use('/api/rooms', roomRoutes);
